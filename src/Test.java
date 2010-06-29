3088c77f809261048b99d1ac132c01bf6bb30952
javazoom <williamjingronin@gmail.com>
2010-06-29 18:37:14 +0800

    Rejected commit: Default

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 2010-6-29
+ * Time: 18:32:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
