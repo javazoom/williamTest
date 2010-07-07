2411873fbc08759ba7c6e1e3ecc30ff4d9daaccd
javazoom <williamjingronin@gmail.com>
2010-07-07 10:11:37 +0800

    自我时间管理程序

--- timeSchedule/src/william/time/test/TimeSchedule.java
+++ timeSchedule/src/william/time/test/TimeSchedule.java
@@ -1,8 +1,10 @@
 package william.time.test;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Locale;
 
 /**
@@ -33,6 +35,47 @@ public class TimeSchedule {
         calendar.setTime(new Date());
         System.out.println(calendar.getTime());
 
+//       时间运算,年月日的时间搞定，小时分钟秒还没呢；
+            // 声明一个当前时间
+        GregorianCalendar grc = new GregorianCalendar();
+        // 设定一个格式化标准
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        // 对当前时间进行运算 其中第一个参数可以设置进行运算的单位
+        grc.add(GregorianCalendar.YEAR, 1);
+        grc.add(GregorianCalendar.MONTH, 2);
+        grc.add(GregorianCalendar.DATE, 3);
+
+        // 代码原型
+        grc.set(2010,7,7,9,15,0);
+        grc.add(GregorianCalendar.MINUTE, 25);
+
+        int init_hour = 6;
+        int init_minute = 15;
+        int init_second = 0;
+
+        GregorianCalendar init_calendar = new GregorianCalendar();
+
+        init_calendar.set(GregorianCalendar.HOUR, init_hour);
+        init_calendar.set(GregorianCalendar.MINUTE, init_minute);
+        init_calendar.set(GregorianCalendar.SECOND, init_second);
+        
+        int workTime = 25;
+        int resetTime = 5;
+        System.out.println("---------start time-------------");
+            System.out.println(sdf.format(init_calendar.getTime()));
+        for(int i = 1; i < 35; i++)
+        {
+           init_calendar.add(GregorianCalendar.MINUTE, workTime);
+           System.out.println(sdf.format(init_calendar.getTime()));
+           init_calendar.add(GregorianCalendar.MINUTE, resetTime);
+           System.out.println(sdf.format(init_calendar.getTime()));
+
+        }
+
+
+        System.out.println("---------end time-------------");
+
+//        System.out.println(sdf.format(grc.getTime()));
     }
     
 }
